ab50374b94263d9f9361eeee305cb5616f5ed3c7
Tyler Williams <tyler@coolsoftware.dev>
2022-10-04 10:30:50 -0600

    start building out blog index page

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,13 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/img");
   eleventyConfig.addPassthroughCopy("src/css");
 
+  eleventyConfig.addCollection("post", function (collection) {
+    let pages = collection.getFilteredByTag("post");
+    return pages.sort(function (a, b) {
+      return b.date - a.date;
+    });
+  });
+
   return {
     dir: {
       input: "src",
